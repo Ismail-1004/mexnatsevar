5b97c18f2d91cf39c36a8407f13c2dd81e4d402b
Ismail <herheulidzeismail980@gmail.com>
2022-12-12 17:34:14 +0500

    save

---
+++ js/app.js
@@ -0,0 +1,164 @@
+//Forked by https://codepen.io/giana/pen/qbWNYy
+
+/* canvas start */
+
+"use strict";
+
+var canvas = document.getElementById('header__canvas'),
+  ctx = canvas.getContext('2d'),
+  w = canvas.width = window.innerWidth,
+  h = canvas.height = window.innerHeight,
+
+  hue = 217,
+  stars = [],
+  count = 0,
+  maxStars = 1400;
+
+  
+var canvas2 = document.createElement('canvas'),
+  ctx2 = canvas2.getContext('2d');
+canvas2.width = 100;
+canvas2.height = 100;
+var half = canvas2.width / 2,
+  gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
+gradient2.addColorStop(0.025, '#fff');
+gradient2.addColorStop(0.1, 'hsl(' + hue + ', 61%, 33%)');
+gradient2.addColorStop(0.25, 'hsl(' + hue + ', 64%, 6%)');
+gradient2.addColorStop(1, 'transparent');
+
+ctx2.fillStyle = gradient2;
+ctx2.beginPath();
+ctx2.arc(half, half, half, 0, Math.PI * 2);
+ctx2.fill();
+
+// End cache
+
+function random(min, max) {
+  if (arguments.length < 2) {
+    max = min;
+    min = 0;
+  }
+
+  if (min > max) {
+    var hold = max;
+    max = min;
+    min = hold;
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function maxOrbit(x, y) {
+  var max = Math.max(x, y),
+    diameter = Math.round(Math.sqrt(max * max + max * max));
+  return diameter / 2;
+}
+
+var Star = function () {
+
+  this.orbitRadius = random(maxOrbit(w, h));
+  this.radius = random(60, this.orbitRadius) / 12;
+  this.orbitX = w / 2;
+  this.orbitY = h / 2;
+  this.timePassed = random(0, maxStars);
+  this.speed = random(this.orbitRadius) / 50000;
+  this.alpha = random(2, 10) / 10;
+
+  count++;
+  stars[count] = this;
+}
+
+Star.prototype.draw = function () {
+  var x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX,
+    y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY,
+    twinkle = random(10);
+
+  if (twinkle === 1 && this.alpha > 0) {
+    this.alpha -= 0.05;
+  } else if (twinkle === 2 && this.alpha < 1) {
+    this.alpha += 0.05;
+  }
+
+  ctx.globalAlpha = this.alpha;
+  ctx.drawImage(canvas2, x - this.radius / 2, y - this.radius / 2, this.radius, this.radius);
+  this.timePassed += this.speed;
+}
+
+for (var i = 0; i < maxStars; i++) {
+  new Star();
+}
+
+function animation() {
+  ctx.globalCompositeOperation = 'source-over';
+  ctx.globalAlpha = 0.8;
+  ctx.fillStyle = 'hsla(' + hue + ', 64%, 6%, 1)';
+  ctx.fillRect(0, 0, w, h)
+
+  ctx.globalCompositeOperation = 'lighter';
+  for (var i = 1, l = stars.length; i < l; i++) {
+    stars[i].draw();
+  };
+
+  window.requestAnimationFrame(animation);
+}
+
+animation();
+
+/* canvas end */
+/* ************************ */
+
+
+/* slider start */
+
+const slider = document.querySelector('.header__main-slider')
+const sliderLine = document.querySelector('.header__main-sliderLine')
+const slides = document.querySelectorAll('.header__main-slide')
+const next = document.querySelector('.next')
+const prev = document.querySelector('.prev')
+
+let counter = 0
+let autopaly = true
+let width;
+
+function init () {
+    width = slider.offsetWidth
+    sliderLine.style.width = width * slides.length + 'px'
+
+    slides.forEach(el => {
+        el.style.width = width + 'px'
+    })
+}
+
+init()
+
+
+function rollSlider () {
+    sliderLine.style.transform = `translate(-${counter * slides[0].offsetWidth}px)`
+}
+
+next.addEventListener('click', function () {
+
+    if (counter === slides.length - 1) {
+        counter = -1
+    }
+
+    if (counter !== slides.length - 1) {
+        counter++
+        rollSlider()
+    }
+})
+
+prev.addEventListener('click', function () {
+    if (counter === 0) {
+        counter = slides.length
+    }
+
+    if (counter > 0) {
+        counter--
+        rollSlider()
+    }
+})
+
+/* slider end */
+/* ************************ */
+
